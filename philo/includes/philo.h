41fef128ec88eb048599c06b087b2e3b96933337
Hare Cherrie <hcherrie@et-j1.kzn.21-school.ru>
2021-09-15 01:36:45 +0300

    sec

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -8,4 +8,27 @@
 # include <sys/time.h>
 # include <pthread.h>
 
+typedef struct s_node {
+	char *next;
+	char *prev;
+} t_node;
+
+typedef struct s_circle_list {
+	t_node *head;
+	t_node *tail;
+} t_circle_list;
+
+typedef struct s_philo {
+	int			name;
+	unsigned	left_fork;
+	unsigned	right_fork;
+	t_table		*table;
+} t_philo;
+
+typedef struct s_table {
+	pthread_mutex_t *forks[PHT_SIZE];
+	t_c_list *circle;
+	t_philo	*philo;
+} t_table;
+
 #endif
